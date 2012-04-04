f1cc290b3613f1cb65124888675e325cc6104dab
JD <mybigbrain@gmail.com>
2012-04-03 21:39:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arenas::Application.config.secret_token = '1df048b2dfbdbe461d1d0740bee207ea44bc846f6555191a848c5c11f069f52b470c4cd53e1d26eab488c69de53c3b9bce3d4545a2b09f392bd9cce202555efe'
